6571bb8b54c545cae4699c8bab97dfe253af664f
VikraSelpian <149685364+VikraSelpian@users.noreply.github.com>
2024-02-27 20:21:47 +0700

    menambah file login di controller dan view

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+
+        if (auth()->attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        
+        return redirect()->back()->with('error', 'Username atau password salah.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 
-Route::get('/', function () {
+Route::get('/welcome', function (){
     return view('welcome');
 });
-
-Route::get ('/', [HomeController::class, 'index']);
-Route::get('/contact', [HomeController::class, 'contact']);
