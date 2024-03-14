b433123d2d344feb9a2003475d4bb6a010d4a75d
Cristov Manurung <CristovRM2@gmail.com>
2024-03-14 20:15:20 +0700

    Mengedit/menambahkan file css dan halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Bengkel Motor - Halaman Utama</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            text-align: center;
-            margin: 0;
-            padding: 0;
-        }
-
-        h1 {
-            color: #333;
-            margin-top: 100px;
-        }
-
-        a {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 4px;
-            margin-top: 20px;
-            transition: background-color 0.3s ease;
-        }
-
-        a:hover {
-            background-color: #0056b3;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistem Informasi Bengkel Motor</title>
+  <link href="{{ asset('styles/style.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1>Selamat datang di Bengkel Motor</h1>
-    <a href="{{ route('login') }}">Login Untuk Kehalaman Dashboard</a>
-    <!-- Tambahkan elemen HTML sesuai kebutuhan -->
+  <header>
+    <h1>Sistem Informasi Bengkel Motor</h1>
+    <nav>
+      <ul>
+        <li>Layanan</li>
+        <li>Tentang Kami</li>
+        <li>Kontak</li>
+        <li><a href="{{ route('login') }}">Dashboard</a></li>
+      </ul>
+    </nav>
+  </header>
+  <p>Ini adalah halaman home yang belum sepenuhnya jadi</p>
+  <footer>
+    <p>&copy; 2024 Bengkel Motor. Hak Cipta Dilindungi.</p>
+  </footer>
 </body>
 </html>
