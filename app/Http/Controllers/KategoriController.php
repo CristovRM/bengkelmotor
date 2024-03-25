90d71de0bd1b07b88e105dce69b92a4c9aa90d14
VikraSelpian <149685364+VikraSelpian@users.noreply.github.com>
2024-03-25 20:30:06 +0700

    tambah kategoricontroller

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Kategori;
+
+class KategoriController extends Controller
+{
+    public function index()
+    {
+        $kategori = Kategori::all();
+        return view('kategori.index', compact('kategori'));
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_kategori' => 'required|unique:kategori|max:255',
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori added successfully!');
+    }
+
+    public function show(Category $kategori)
+    {
+        return view('kategori.show', compact('kategori'));
+    }
+
+    public function edit(Kategori $kategori)
+    {
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    public function update(Request $request, Kategori $kategori)
+    {
+        $request->validate([
+            'nama_kategori' => 'required|max:255',
+        ]);
+
+        $kategori->update($request->all());
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori updated successfully!');
+    }
+
+    public function destroy(kategori $kategori)
+    {
+        $kategori->delete();
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori deleted successfully!');
+    }
+}
