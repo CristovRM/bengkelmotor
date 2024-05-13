021d40cd941177ef42b82fe0bf1c341d03a87531
VikraSelpian <149685364+VikraSelpian@users.noreply.github.com>
2024-05-13 19:19:30 +0700

    menambahkan rest api serve untuk kategori,produk dan supplier

---
+++ app/Http/Controllers/API/SupplierController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Supplier;
+use Illuminate\Support\Facades\Validator;
+
+class SupplierController extends Controller
+{
+    public function index()
+    {
+        $data = Supplier::orderby('nama_supplier','asc')->get();
+        return response()->json([
+            'status' => true,
+            'massage' => 'Data Ditemukan',
+            'data' => $data
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+       $dataSupplier = new Supplier;
+
+       $rules = [
+        'nama_supplier' => 'required',
+        'alamat' => 'required',
+       ];
+       $validator = Validator::make($request->all(), $rules);
+       if ($validator->fails()) {
+        return response()->json([
+            'status' => false,
+            'massage' => 'Gagal Memasukkan Data',
+            'data' => $validator->errors()
+
+        ]);
+
+        
+       }
+       
+       $dataSupplier->nama_supplier = $request->nama_supplier;
+       $dataSupplier->alamat = $request->alamat;
+       
+       $post = $dataSupplier->save();
+
+       return response()->json([
+        'status' => true,
+        'massage' => 'Sukses Memasukkan Data',
+
+       ]);
+
+    }
+
+    public function show(string $id)
+    {
+        $data = Supplier::find($id);
+        if ($data) {
+            return response()->json([
+                'status' => true,
+                'massage' => 'Data Ditemukan',
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'massage' => 'Data tidak Ditemukan'
+            ]);
+        }
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $dataSupplier = Supplier::find($id);
+        if (empty($dataSupplier)) {
+            return response()->json([
+                'status' => false,
+                'massage' => 'Data tidak ditemukan',
+
+            ], 404);
+        }
+
+       $rules = [
+        'nama_supplier' => 'required',
+        'alamat' => 'required',
+       ];
+       $validator = Validator::make($request->all(), $rules);
+       if ($validator->fails()) {
+        return response()->json([
+            'status' => false,
+            'massage' => 'Gagal melakukan update data',
+            'data' => $validator->errors()
+
+        ]);
+
+        
+       }
+       
+       $dataSupplier->nama_supplier = $request->nama_supplier;
+       $dataSupplier->alamat = $request->alamat;
+       
+       $post = $dataSupplier->save();
+
+       return response()->json([
+        'status' => true,
+        'massage' => 'Sukses melakukan update data',
+
+       ]);
+    }
+
+    public function destroy(string $id)
+    {
+        $dataSupplier = Supplier::find($id);
+        if (empty($dataSupplier)) {
+            return response()->json([
+                'status' => false,
+                'massage' => 'Data tidak ditemukan',
+
+            ], 404);
+        }
+       
+       $post = $dataSupplier->delete();
+
+       return response()->json([
+        'status' => true,
+        'massage' => 'Sukses melakukan delete data',
+
+       ]);
+    }
+}
